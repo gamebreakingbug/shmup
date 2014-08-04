ef4f98922467c1191b5b3f9811936ae47afa5d36
gamebreakingbug <mediaapplied@medias-imac-2.home>
2014-08-04 09:56:20 +0100

    1

--- Assets/ControllerClass.js
+++ Assets/ControllerClass.js
@@ -2,121 +2,59 @@
 
 	  var window : GameObject;
 
-  	enum State{Cruising, Climbing, Diving, BankingL, BankingR, RollingL, RollingR};
+  	enum State{Cruising, Climbing, Diving, BankingL, BankingR, RollingL, RollingR, Boosting};
 	  enum Depth{Bottom, Middle, Top};
 
 public class ControllerClass extends Moveables {
 
-    var tCr : Sprite;
-    var tBl : Sprite;
-    var tBr : Sprite;
-    var tDv1 : Sprite;
-    var tDv2 : Sprite;
-
-  	var mCr : Sprite;
-  	var mBl : Sprite;
-  	var mBr : Sprite;
-    var mCl1 : Sprite;
-    var mCl2 : Sprite;
-    var mDv1 : Sprite;
-    var mDv2 : Sprite;
-
-    var bCr : Sprite;
-    var bBl : Sprite;
-    var bBr : Sprite;
-    var bCl1 : Sprite;
-    var bCl2 : Sprite;
-
     private var stateHashtable : Hashtable = new Hashtable();
   	private var bounce : boolean;
-    private var rolling : boolean;
     private var transitioning : boolean;
 
   	var state : State;
     var depth : Depth;
-  	var thrust : float;
+
+    var thrust : float;
   	var bank : float;
     var pitch : float;
-
-    function Start(){
-
-      var topHashtable : Hashtable = new Hashtable();
-      var tDvArray = [tDv1, tDv2, mCr];
-      topHashtable[State.Cruising] = tCr;
-      topHashtable[State.BankingL] = tBl;
-      topHashtable[State.BankingR] = tBr;
-      topHashtable[State.Diving] = tDvArray;
-
-      var middleHashtable : Hashtable = new Hashtable();
-      var mClArray = [mCl1, mCl2, tCr];
-      var mDvArray = [mDv1, mDv2, bCr];
-      middleHashtable[State.Cruising] = mCr;
-      middleHashtable[State.BankingL] = mBl;
-      middleHashtable[State.BankingR] = mBr;
-      middleHashtable[State.Climbing] = mClArray;
-      middleHashtable[State.Diving] = mDvArray;
-
-      var bottomHashtable : Hashtable = new Hashtable();
-      var bClArray = [bCl1, bCl2, mCr];
-      bottomHashtable[State.Cruising] = bCr;
-      bottomHashtable[State.BankingL] = bBl;
-      bottomHashtable[State.BankingR] = bBr;
-      bottomHashtable[State.Climbing] = bClArray;
-
-      stateHashtable[Depth.Top] = topHashtable;
-      stateHashtable[Depth.Middle] = middleHashtable;
-      stateHashtable[Depth.Bottom] = bottomHashtable;
-
-      depth = Depth.Bottom;
-    }
-
+    var roll : float;
 
   	function CheckInput(){
 
     	  thrust = Input.GetAxis("Vertical");
     	  bank = Input.GetAxis("Horizontal");
         pitch = Input.GetAxis("Vertical2");
+        roll = Input.GetAxis("Horizontal2");
 
-    		if(!bounce){
-
-          if(!rolling && depth != depth.Top && Input.GetButtonDown("LeftBumper") && state != State.Climbing){
-            state = State.Climbing;
-            //BarrelRoll("LeftBumper");
+        if (pitch == -1 && depth != depth.Top && state == State.Cruising){
+          state = State.Climbing;
           }
-          else if(!rolling && depth != depth.Bottom &&  Input.GetButtonDown("RightBumper") && state != State.Diving){
-            state = State.Diving;
-            //BarrelRoll("RightBumper");
+        else if (pitch == 1 && depth != depth.Bottom && state == State.Cruising){
+          state = State.Diving;
           }
-          else if (!rolling){
-    		    SetMove(bank,thrust);
+        else if (roll == 1 && (state == State.Cruising || state == State.BankingR)){
+          state = State.RollingR;
           }
+        else if (roll == -1  && (state == State.Cruising || state == State.BankingL)){
+          state = State.RollingL;
+          }
+        else if (state == State.Cruising && Input.GetButtonDown("Boost")){
+          state = State.Boosting;
+        }
+        else if (bank >= 0.4){
+          state = State.BankingR;
+          }
+        else if (bank <= -0.4){
+          state = State.BankingL;
+          }
+        else {
+          state = State.Cruising;
         }
 
+        if(!bounce){
+          SetMove(bank,thrust);
+        }
   	}
-
-    function BarrelRoll(bumper : String){
-
-        var rollSpeed : float;
-
-        switch(bumper){
-          case "LeftBumper":
-            rollSpeed = speed * -4;
-            break;
-          case "RightBumper":
-            rollSpeed = speed * 4;
-            break;
-          default:
-            break;
-        }
-        rolling = true;
-        SetVelocity(rollSpeed , velocity.y);
-        while(Input.GetButton(bumper)){
-          SetVelocity(velocity.x * 0.95f, velocity.y);
-          yield;
-        }
-          rolling = false;
-    }
-
 
   	function ScreenCheck(){
 
@@ -125,7 +63,7 @@ public class ControllerClass extends Moveables {
 
   			screenBounds = Vector3(Screen.width,Screen.height,0);
   			screenOffset.x = (screenBounds.x*6)/100; // 6% of Screen width
-  			screenOffset.y = (screenBounds.y*12)/100; // 6% of Screen height
+  			screenOffset.y = (screenBounds.y*6)/100; // 6% of Screen height
 
   			var currentPos : Vector3 = Camera.main.WorldToScreenPoint(transform.position);
 
@@ -146,6 +84,7 @@ public class ControllerClass extends Moveables {
         // Shake(0.2f,0.1f);
 
     		if(contact == Edge.Horiz){
+
     			SetVelocity(velocity.x * -1.0f, velocity.y); // flip horizontal direction
     			while(bank !=0){
     				velocity *= 0.95f;
@@ -154,6 +93,7 @@ public class ControllerClass extends Moveables {
     		}
 
     		else {
+
     			SetVelocity(velocity.x, velocity.y * -1.0f); // flip vertical direction
     			while(thrust !=0){
     				velocity *= 0.95f;
@@ -165,12 +105,37 @@ public class ControllerClass extends Moveables {
 
   	}
 
-    function ChangeDepth(array : Array){
+    function Transitioning(array : Array){
 
         var cameraSize : float = Camera.main.orthographicSize;
         var renderer : SpriteRenderer = gameObject.GetComponent(SpriteRenderer);
         var currentSpr : Sprite;
+        var sizeMod : float; // used for climbing / diving
+        var rollSpeed : float = 30;
+        var boostSpeed : float = 20;
         var loop : int = array.length - 1;
+        var newPos : Vector2;
+        var d : int = depth;
+        var depthMod : float = 0.5 + (0.25 * d);
+
+
+        if(state == State.RollingR){
+
+            newPos =  Vector2(((speed * rollSpeed)/cameraSize)/100,0);
+            transform.position.x += cameraSize * (newPos.x * depthMod);
+            SetVelocity(speed,0);
+            }
+        else if(state == State.RollingL){
+
+            newPos =  Vector2(((speed * rollSpeed)/cameraSize)/100,0);
+            transform.position.x += cameraSize * (-newPos.x * depthMod);
+            SetVelocity(-speed,0);
+            }
+        else if(state == State.Boosting){
+              newPos =  Vector2(0,((speed * boostSpeed)/cameraSize)/100);
+              transform.position.y += cameraSize * (newPos.y * depthMod);
+              SetVelocity(0, speed);
+              }
 
         for(var i = 0; i<loop; i++){
           currentSpr = array[i];
@@ -181,33 +146,41 @@ public class ControllerClass extends Moveables {
         currentSpr = array[loop];
         renderer.sprite = currentSpr;
 
-        var sizeMod : float;
-
         if(state == State.Climbing){
-          if(depth == Depth.Bottom){
 
-            sizeMod =  ( 52 / cameraSize ) / 100; // + 52px
+            sizeMod =  ( 28 / cameraSize ) / 100; //  + 20 px (+ 36 px)
             transform.position.y += cameraSize * sizeMod;
-          }
-          else if(depth == Depth.Middle){
+            depth += 1;
 
-            sizeMod =  ( 36 / cameraSize ) / 100; // + 36 px
-            transform.position.y += cameraSize * sizeMod;
-          }
-          depth += 1;
         }
+
         else if(state == State.Diving){
+
           if(depth == Depth.Top){
 
-            sizeMod =  ( 52 / cameraSize ) / 100; // + 52px
-            transform.position.y += cameraSize * sizeMod; // +
-          }
-          else if(depth == Depth.Middle){
-
-            sizeMod =  ( 68 / cameraSize ) / 100; // + 68px
+            sizeMod =  ( 48 / cameraSize ) / 100; // + 48 px (+ 52 px)
             transform.position.y += cameraSize * sizeMod;
           }
+
+          else if(depth == Depth.Middle){
+
+            sizeMod =  ( 32 / cameraSize ) / 100; // + 52 px (+ 68 px)
+            transform.position.y += cameraSize * sizeMod;
+          }
+
           depth -= 1;
+
+        }
+
+        if(state == State.RollingL || state == State.RollingR){
+            SetVelocity(velocity.x * -0.1f, velocity.y);
+
+        }
+
+        if(state == State.Boosting){
+            sizeMod =  ( 28 / cameraSize ) / 100; //  + 20 px (+ 36 px)
+            transform.position.y += cameraSize * sizeMod;
+            SetVelocity(velocity.x, velocity.y  * -0.1f);
         }
 
         transitioning = false;
@@ -222,10 +195,9 @@ public class ControllerClass extends Moveables {
 
         if(!transitioning){
 
-            if(state == State.Climbing || state == State.Diving){
-
+            if(state == State.Climbing || state == State.Diving || state == State.RollingL || state == State.RollingR || state == State.Boosting){
               transitioning = true;
-              ChangeDepth(currentHashtable[state]);
+              Transitioning(currentHashtable[state]);
 
             }
 
@@ -233,31 +205,76 @@ public class ControllerClass extends Moveables {
 
               currentSpr = currentHashtable[state];
           		var renderer : SpriteRenderer = gameObject.GetComponent(SpriteRenderer);
-
-
-                if(velocity.x < 0.4 && velocity.x > -0.4){
-                  state = State.Cruising;
-                }
-                else if (bank >= 0.4){
-          				state = State.BankingR;
-          				}
-          			else if (bank <= -0.4){
-          				state = State.BankingL;
-          				}
-          			else{
-          				state = State.Cruising;
-          			}
-          		renderer.sprite = currentSpr;
+      		    renderer.sprite = currentSpr;
 
             }
         }
   	}
 
+
+    function Start(){
+
+      var Spr : Sprite[] = Resources.LoadAll.<Sprite>("Images/new-junker");
+
+      var topHashtable : Hashtable = new Hashtable();
+      var tDVarray = [Spr[3],Spr[13],Spr[1]];
+      var tRLarray = [Spr[24],Spr[28],Spr[4]];
+      var tRRarray = [Spr[32],Spr[16],Spr[20]];  //  var tRRarray = [Spr[16],Spr[20],Spr[8]];
+      var tBSarray = [Spr[23],Spr[12],Spr[0]];
+
+      topHashtable[State.Cruising] = Spr[0];
+      topHashtable[State.BankingL] = Spr[4];
+      topHashtable[State.BankingR] = Spr[8];
+      topHashtable[State.Diving] = tDVarray;
+      topHashtable[State.Boosting] = tBSarray;
+      topHashtable[State.RollingL] = tRLarray;
+      topHashtable[State.RollingR] = tRRarray;
+
+      var middleHashtable : Hashtable = new Hashtable();
+      var mDVarray = [Spr[11],Spr[14],Spr[2]];
+      var mCMarray = [Spr[7],Spr[12],Spr[0]];
+      var mRLarray = [Spr[25],Spr[29],Spr[5]];
+      var mRRarray = [Spr[17],Spr[21],Spr[9]];
+      var mBSarray = [Spr[27],Spr[13],Spr[1]];
+
+      middleHashtable[State.Cruising] = Spr[1];
+      middleHashtable[State.BankingL] = Spr[5];
+      middleHashtable[State.BankingR] = Spr[9];
+      middleHashtable[State.Diving] = mDVarray;
+      middleHashtable[State.Climbing] = mCMarray;
+      middleHashtable[State.Boosting] = mBSarray;
+      middleHashtable[State.RollingL] = mRLarray;
+      middleHashtable[State.RollingR] = mRRarray;
+
+      var bottomHashtable : Hashtable = new Hashtable();
+      var bCMarray = [Spr[15],Spr[19],Spr[1]];
+      var bRLarray = [Spr[26],Spr[30],Spr[6]];
+      var bRRarray = [Spr[18],Spr[22],Spr[10]];
+      var bBSarray = [Spr[31],Spr[14],Spr[2]];
+
+      bottomHashtable[State.Cruising] = Spr[2];
+      bottomHashtable[State.BankingL] = Spr[6];
+      bottomHashtable[State.BankingR] = Spr[10];
+      bottomHashtable[State.Climbing] = bCMarray;
+      bottomHashtable[State.Boosting] = bBSarray;
+      bottomHashtable[State.RollingL] = bRLarray;
+      bottomHashtable[State.RollingR] = bRRarray;
+
+      stateHashtable[Depth.Top] = topHashtable;
+      stateHashtable[Depth.Middle] = middleHashtable;
+      stateHashtable[Depth.Bottom] = bottomHashtable;
+
+      depth = Depth.Middle;
+
+    }
+
     function FixedUpdate () {
 
-        window.GetComponent(GUIText).text = "velocity = " + velocity + "\n" + "thrust = " + thrust + "\n" + "bank = " + bank + "\n" + "depth = " + depth + "\n" + "state = " + state;
+        window.GetComponent(GUIText).text = "velocity = " + velocity + "\n" + "thrust = " + thrust + "\n" + "bank = " + bank + "\n" + "pitch = " + pitch + "\n" + "roll = " + roll + "\n" + "depth = " + depth + "\n" + "state = " + state + "\n" + "transitioning = " + transitioning;
 
-        CheckInput();
+        if(!transitioning){
+          CheckInput();
+        }
         Move(velocity);
         ScreenCheck();
         SpriteRend();
